71605d7a4f02dc951661460c29bd19709ce58c22
jacob-gerun <77089997+jacob-gerun@users.noreply.github.com>
2022-10-26 17:52:24 -0600

    hasCity

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -52,5 +52,7 @@ public class CustomList extends ArrayAdapter<City> {
     public void addCity(City city){
         cities.add(city);
     }
-
+    public boolean hasCity(City city) {
+        return cities.contains(city);
+    }
 }
